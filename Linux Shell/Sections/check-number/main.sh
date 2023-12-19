f9f89187dcc606d926494d747082c917412bc4f9
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-12-19 18:58:56 +0200

    Create main.sh

---
+++ Linux Shell/Sections/check-number/main.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Enter the number"
+read num
+
+if [ $num -gt 0 ]; then
+   echo "The number is positive"
+elif [ $num -lt 0 ]; then
+   echo "The number is negative"
+else
+   echo "The number is zero"
+fi
+
